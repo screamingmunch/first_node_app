b5579b82d66dc6e43f01591df3e63bfeae5b2100
kristinelai <screamingmunch@gmail.com>
2014-06-05 16:33:44 -0700

    update web.js

--- web.js
+++ web.js
@@ -2,13 +2,23 @@ var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 
-app.use(logfmt.requestLogger());
+// parses and generates log lines in logfmt style
+app.use(logfmt.requestLogger()); 
+
 
 app.get('/', function(request, response) {
   response.send("Hello Dawg");
 });
 
-var port = Number(process.env.PORT || 5000); //Foreman defaults $port env var @ port 5000
+
+//Foreman defaults $port env var @ port 5000
+var port = Number(process.env.PORT || 5000); 
 app.listen(port, function(){
   console.log("listening on port " + port);
 });
+
+// var server = http.createServer(app);
+// var port = process.env.PORT || 5000;
+// server.listen(port, function(){
+//   console.log("listening on port" + port);
+// });
