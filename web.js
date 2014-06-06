f30edeaccebbcb86c4ce7223adf7632333a36e53
kristinelai <screamingmunch@gmail.com>
2014-06-06 16:42:25 -0700

    initial readme

--- web.js
+++ web.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var logfmt = require("logfmt");
+var http = require('http');
 var app = express();
+var server = http.createServer(app);
 
 // parses and generates log lines in logfmt style
 app.use(logfmt.requestLogger()); 
@@ -11,14 +13,15 @@ app.get('/', function(request, response) {
 });
 
 
-//Foreman defaults $port env var @ port 5000
-var port = Number(process.env.PORT || 5000); 
-app.listen(port, function(){
-  console.log("listening on port " + port);
-});
 
-// var server = http.createServer(app);
-// var port = process.env.PORT || 5000;
-// server.listen(port, function(){
-//   console.log("listening on port" + port);
-// });
+
+
+
+//Foreman defaults $port env var @ port 5000
+var port = Number(process.env.PORT || 5000);
+server.listen(port, function(){
+//app.listen(port, function(){})  also works, it makes express create an http server for you
+//server.listen creates a http server yourself, it's useful when you want to reuse the http server
+// for example, to run socket.io within the same http server instance
+  console.log("listening on port" + port);
+});
